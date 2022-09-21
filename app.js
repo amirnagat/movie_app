8c73cbd78653cd001f167965e522204c3691543b
amir <nagat1700@gmail.com>
2022-09-21 22:56:00 +0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// window.addEventListener('load',function(){
-// 	setTimeout(() => {
-// 		console.log("Delayed for 1 second.");
-// 	  }, "3000")
-// })
 
 
 const options = {
@@ -30,7 +25,6 @@ fetch("https://movies-app1.p.rapidapi.com/api/movies", options)
 		let filterIt = response.results.filter(function (movie) {
 		   return movie.titleOriginal.toLowerCase().includes(inputVal.toLowerCase());
 		});
-		// showData(filterIt);
 	  showData(filterIt);
 	});
   });
@@ -40,7 +34,6 @@ fetch("https://movies-app1.p.rapidapi.com/api/movies", options)
 
 function showData(arrTitle) {
   arrTitle.forEach(function (title, index) {
-    // console.log(title.image.url);
     allMovies.style.cssText = `  overflow-x: hidden;`;
     movie.innerHTML += `<div class="warp col-3 justify-content-center align-items-baseline d-flex flex-column ">
 		<img class="w-75" src=${title.image} width:20px;  alt="#">
