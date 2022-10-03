ee4b46e147440225b1649eda7defaae430fea2d6
amir <nagat1700@gmail.com>
2022-10-03 13:07:46 +0300

    update

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const options = {
     "X-RapidAPI-Host": "movies-app1.p.rapidapi.com",
   },
 };
-let input = document.querySelector("input");
-let body = document.body;
+let input = document.querySelector("input"); 
+let body = document.body; 
 let allMovies = document.createElement("div");
 let movie = document.createElement("div");
 body.append(allMovies);
